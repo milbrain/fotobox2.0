f053dd38b898c96830479214c15c9570a36b468b
Bartosz Milejski <mil_bar@hotmail.de>
2019-04-14 19:45:02 +0200

    Replace impractical tuple-structues by custom class with record-like functionality; little code refactoring for better readability

--- GUI.py
+++ GUI.py
@@ -8,6 +8,8 @@ from PyQt5.QtWidgets import QLabel, QFrame
 from PyQt5.QtCore import QRect, QPoint, QDir#, pyqtSignal
 from PyQt5.Qt import *
 
+class Record:
+    pass
 
 class GUI(QMainWindow):
     
@@ -17,14 +19,14 @@ class GUI(QMainWindow):
     imageFilepath   = 'res/img/'
     previewFilepath = 'res/preview/'
     
-    formSize = QPoint(1000,700)                # main form
+    formSize = QPoint(1000,700)         # main form
     
     previewImageSize = QPoint(165, 165)
     totalPreviewImages = 5
     
     bigImageSize    = None              # is only set after the first call to _setupBigImage
     
-    bigLabel        = (0, 0)            # Tuple with reference to a QLabel and the ImageNr currently stored there
+    bigLabel        = None              # Record with reference to a QLabel and the ImageNr currently stored there
                                         # only available after _setupBigImage
     preLabels       = []                # List of tuples of the same form as bigLabel, just for the preview labels
                                         # only available after _setupPreviewImages
@@ -60,125 +62,6 @@ class GUI(QMainWindow):
         elif event.key() == QtCore.Qt.Key_Right:
             self.scrollRight()
             
-        
-    def scrollLeft(self):
-        newImgNr = self.bigLabel[1] - 1
-        
-        # bigPreviewImage
-        if newImgNr <= 0 or not self.loadBigImage(newImgNr, self.bigImageSize.x(), self.bigImageSize.y()):
-            return
-        
-        # QR-Code
-        self.generateOrLoadQR(newImgNr)
-        
-        # set new Image Number because everything worked out
-        self.bigLabel = (self.bigLabel[0], newImgNr)
-        
-        # Realign small preview images (if neccessary)
-        imgDir = QDir(self.imageFilepath)
-        existingImages = imgDir.count() - 2
-        if (newImgNr < math.ceil(self.totalPreviewImages/2) or
-            existingImages <= self.totalPreviewImages):
-            return
-        
-        for i in range(self.totalPreviewImages-1, 0,-1):
-            pixmap = self.preLabels[i-1][0].pixmap()
-            if pixmap:
-                self.preLabels[i][0].setPixmap(pixmap)
-                self.preLabels[i] = (self.preLabels[i][0], self.preLabels[i-1][1])
-
-        # Load new picture into leftmost preview label
-        pixmap = self.generatePreviewImage(newImgNr - math.floor(self.totalPreviewImages/2))
-        if pixmap:
-            self.preLabels[0][0].setPixmap(pixmap)
-            self.preLabels[0] = (self.preLabels[0][0], newImgNr - math.floor(self.totalPreviewImages/2))
-        
-        # === DEBUG ===
-        pstr = ""
-        for i in range(len(self.preLabels)):
-            pstr += str(self.preLabels[i][1]) + " "
-        print("Scrolling finished. Position is: " + pstr)
-
-    def scrollRight(self):
-        newImgNr = self.bigLabel[1] + 1
-        
-        # bigPreviewImage
-        if newImgNr <= 0 or not self.loadBigImage(newImgNr, self.bigImageSize.x(), self.bigImageSize.y()):
-            return
-        
-        # QR-Code
-        self.generateOrLoadQR(newImgNr)
-        
-        # set new Image Number because everything worked out
-        self.bigLabel = (self.bigLabel[0], newImgNr)
-        
-        # Realign small preview images (if neccessary)
-        imgDir = QDir(self.imageFilepath)
-        existingImages = imgDir.count() - 2
-        if (newImgNr <= math.ceil(self.totalPreviewImages/2) or
-            existingImages <= self.totalPreviewImages):
-            return
-        
-        for i in range(0,self.totalPreviewImages-1, 1):
-            pixmap = self.preLabels[i+1][0].pixmap()
-            if pixmap:
-                self.preLabels[i][0].setPixmap(pixmap)
-                self.preLabels[i] = (self.preLabels[i][0], self.preLabels[i+1][1])
-                #self.preLabels[i][0].setText(str(self.preLabels[i][1])) #t1
-            else:
-                pix = QPixmap(self.previewImageSize.x(), self.previewImageSize.y())
-                pix.fill(QColor(0,0,0,0))
-                self.preLabels[i][0].setPixmap(pix)
-                self.preLabels[i] = (self.preLabels[i][0], 0)
-
-        # Load new picture into rightmost preview label
-        pixmap = self.generatePreviewImage(newImgNr + math.floor(self.totalPreviewImages/2))
-        if pixmap:
-            self.preLabels[self.totalPreviewImages-1][0].setPixmap(pixmap)
-            self.preLabels[self.totalPreviewImages-1] = (self.preLabels[self.totalPreviewImages-1][0],
-                                                         newImgNr + math.floor(self.totalPreviewImages/2))
-            #self.preLabels[self.totalPreviewImages-1][0].setText(str(self.preLabels[self.totalPreviewImages-1][1])) #t1
-        else:
-            pix = QPixmap(self.previewImageSize.x(), self.previewImageSize.y())
-            pix.fill(QColor(0,0,0,0))
-            self.preLabels[self.totalPreviewImages-1][0].setPixmap(pix)
-            self.preLabels[self.totalPreviewImages-1] = (self.preLabels[self.totalPreviewImages-1][0], 0)
-
-        # === DEBUG ===
-        pstr = ""
-        for i in range(len(self.preLabels)):
-            pstr += str(self.preLabels[i][1]) + " "
-        print("Scrolling finished. Position is: " + pstr)
-        
-    def _setupBigImage(self):
-        ''' Load newest picture into the bigLabel. '''
-        bigWidth = self.formSize.x() - 165 - 10 - 20 # qrWidth, border, inBetweenSpace
-        bigHeight = self.formSize.y() - self.previewImageSize.y() - 10 - 20
-        size = QRect(10, 10, bigWidth, bigHeight)
-        self.bigImageSize = QPoint(bigWidth, bigHeight)
-        
-        imgDir = QDir(self.previewFilepath)
-        existingImages = imgDir.count() - 2
-        
-        self.bigLabel = (QLabel(self), 0)
-        self.bigLabel[0].setGeometry(size)
-        self.bigLabel[0].setAlignment(Qt.AlignCenter | Qt.AlignVCenter)
-        self.bigLabel[0].show()
-        self.loadBigImage(existingImages, bigWidth, bigHeight)   
-        
-    def loadBigImage(self, imgNr, w, h):
-        ''' Loads the big preview label with the existing image referenced by imgNr. '''
-        # If permance is too low, change QtSmoothTransformation
-        filepath = self.imageFilepath + 'P' + str(imgNr) + '.jpg'
-        if not os.path.isfile(filepath):
-            return False
-        
-        img = QImage(filepath)
-        pixmap = QtGui.QPixmap.fromImage(img.scaled(w, h, Qt.KeepAspectRatio, Qt.SmoothTransformation))
-        self.bigLabel[0].setPixmap(pixmap)
-        self.bigLabel = (self.bigLabel[0], imgNr)
-        return True    
-        
     def _setupPreviewImages(self):
         ''' Loads all preview images into the form. '''
         formWidth = self.formSize.x()
@@ -204,27 +87,32 @@ class GUI(QMainWindow):
             # Create as many labels as requested in totalPreviewImages
             size = QRect(10 + i*preWidth + inBetweenSpace*i, formHeigth - preHeight - 10,
                          preWidth, preHeight)
-            self.preLabels.append((QtWidgets.QLabel(self), i+1))
-            self.preLabels[i][0].setGeometry(size)
-            self.preLabels[i][0].setAlignment(Qt.AlignCenter)
+            
+            preview = Record()
+            preview.label = QtWidgets.QLabel(self)
+            preview.imageNr = 0
+            
+            self.preLabels.append(preview)
+            self.preLabels[i].label.setGeometry(size)
+            self.preLabels[i].label.setAlignment(Qt.AlignCenter)
             
             # Iteratively try to generate the next picture
             pixmap = self.generatePreviewImage(imgToGenerate)
             if pixmap:
-                self.preLabels[i][0].setPixmap(pixmap)
-                self.preLabels[i] = (self.preLabels[i][0], imgToGenerate)
+                self.preLabels[i].label.setPixmap(pixmap)
+                self.preLabels[i].imageNr = imgToGenerate
             else:
                 # No more pictures left to load
-                self.preLabels[i] = (self.preLabels[i][0], 0)
+                self.preLabels[i].imageNr = 0
                 
-            self.preLabels[i][0].show()
+            self.preLabels[i].label.show()
             imgToGenerate += 1
         
         
         # === DEBUG ===
         pstr = ""
         for i in range(len(self.preLabels)):
-            pstr += str(self.preLabels[i][1]) + " "
+            pstr += str(self.preLabels[i].imageNr) + " "
         print("Setting up preview finished. Position is: " + pstr)
         
         # Snippet for future use
@@ -233,8 +121,7 @@ class GUI(QMainWindow):
         #if i == math.ceil(self.totalPreviewImages/2):
         #    self.preLabels[0][0].setFrameShape(QFrame.Panel)
         #self.preLabels[0][0].setFrameShape(QFrame.NoFrame)
-        
-        
+
     def generatePreviewImage(self, imgNr):
         ''' Generates the preview Image according to the parameter imgNr. Returns the generated pixmap or False
         if the generation failed'''
@@ -251,8 +138,38 @@ class GUI(QMainWindow):
                                                     Qt.KeepAspectRatio, Qt.SmoothTransformation))
         pixmap.save(prePath)
         return pixmap
+
+    def _setupBigImage(self):
+        ''' Load newest picture into the bigLabel. '''
+        bigWidth = self.formSize.x() - 165 - 10 - 20 # qrWidth, border, inBetweenSpace
+        bigHeight = self.formSize.y() - self.previewImageSize.y() - 10 - 20
+        size = QRect(10, 10, bigWidth, bigHeight)
+        self.bigImageSize = QPoint(bigWidth, bigHeight)
         
-    
+        imgDir = QDir(self.previewFilepath)
+        existingImages = imgDir.count() - 2
+        
+        self.bigLabel = Record()
+        self.bigLabel.label = QLabel(self)
+        self.bigLabel.imageNr = 0
+        self.bigLabel.label.setGeometry(size)
+        self.bigLabel.label.setAlignment(Qt.AlignCenter | Qt.AlignVCenter)
+        self.bigLabel.label.show()
+        self.loadBigImage(existingImages, bigWidth, bigHeight)   
+        
+    def loadBigImage(self, imgNr, w, h):
+        ''' Loads the big preview label with the existing image referenced by imgNr. '''
+        # If permance is too low, change QtSmoothTransformation
+        filepath = self.imageFilepath + 'P' + str(imgNr) + '.jpg'
+        if not os.path.isfile(filepath):
+            return False
+        
+        img = QImage(filepath)
+        pixmap = QtGui.QPixmap.fromImage(img.scaled(w, h, Qt.KeepAspectRatio, Qt.SmoothTransformation))
+        self.bigLabel.label.setPixmap(pixmap)
+        self.bigLabel.imageNr = imgNr
+        return True    
+       
     def _setupQRCode(self):
         ''' Create the label to show the QR Code for and load the first QR-Code (if at least
         one image exists in the image folder already). '''
@@ -262,10 +179,9 @@ class GUI(QMainWindow):
         self.qrlabel = QtWidgets.QLabel(self)
         self.qrlabel.setGeometry(size)
         
-        self.generateOrLoadQR(self.bigLabel[1])
+        self.generateOrLoadQR(self.bigLabel.imageNr)
         self.qrlabel.show()
         
-    
     def generateOrLoadQR(self, imgNr):
         ''' Loads an existing QR-Code into the UI or generates the QR Code first and
         loads it afterwards. Parameter denotes the image number to load the QR code for.'''
@@ -286,7 +202,98 @@ class GUI(QMainWindow):
         
         pixmap = QtGui.QPixmap(filepath)
         self.qrlabel.setPixmap(pixmap)
-                
+                         
+    def scrollLeft(self):
+        newImgNr = self.bigLabel.imageNr - 1
+        
+        # bigPreviewImage
+        if newImgNr <= 0 or not self.loadBigImage(newImgNr, self.bigImageSize.x(), self.bigImageSize.y()):
+            return
+        
+        # QR-Code
+        self.generateOrLoadQR(newImgNr)
+        
+        # set new Image Number because everything worked out
+        self.bigLabel.imageNr = newImgNr
+        
+        # Realign small preview images (if neccessary)
+        imgDir = QDir(self.imageFilepath)
+        existingImages = imgDir.count() - 2
+        if (newImgNr < math.ceil(self.totalPreviewImages/2) or
+            existingImages <= self.totalPreviewImages):
+            return
+        
+        for i in range(self.totalPreviewImages-1, 0,-1):
+            pixmap = self.preLabels[i-1].label.pixmap()
+            if pixmap:
+                self.preLabels[i].label.setPixmap(pixmap)
+                self.preLabels[i].imageNr = self.preLabels[i-1].imageNr
+
+        # Load new picture into leftmost preview label
+        pixmap = self.generatePreviewImage(newImgNr - math.floor(self.totalPreviewImages/2))
+        if pixmap:
+            self.preLabels[0].label.setPixmap(pixmap)
+            self.preLabels[0].imageNr = newImgNr - math.floor(self.totalPreviewImages/2)
+        
+        # === DEBUG ===
+        pstr = ""
+        for i in range(len(self.preLabels)):
+            pstr += str(self.preLabels[i].imageNr) + " "
+        print("Scrolling finished. Position is: " + pstr)
+
+    def scrollRight(self):
+        newImgNr = self.bigLabel.imageNr + 1
+        
+        # bigPreviewImage
+        if newImgNr <= 0 or not self.loadBigImage(newImgNr, self.bigImageSize.x(), self.bigImageSize.y()):
+            return
+        
+        # QR-Code
+        self.generateOrLoadQR(newImgNr)
+        
+        # set new Image Number because everything worked out
+        self.bigLabel.imageNr = newImgNr
+        
+        # Realign small preview images (if neccessary)
+        imgDir = QDir(self.imageFilepath)
+        existingImages = imgDir.count() - 2
+        if (newImgNr <= math.ceil(self.totalPreviewImages/2) or
+            existingImages <= self.totalPreviewImages):
+            return
+        
+        for i in range(0,self.totalPreviewImages-1, 1):
+            pixmap = self.preLabels[i+1].label.pixmap()
+            if pixmap:
+                self.preLabels[i].label.setPixmap(pixmap)
+                self.preLabels[i].imageNr = self.preLabels[i+1].imageNr
+            else:
+                pix = QPixmap(self.previewImageSize.x(), self.previewImageSize.y())
+                pix.fill(QColor(0,0,0,0))
+                self.preLabels[i].label.setPixmap(pix)
+                self.preLabels[i].imageNr = 0
+
+        # Load new picture into rightmost preview label
+        pixmap = self.generatePreviewImage(newImgNr + math.floor(self.totalPreviewImages/2))
+        if pixmap:
+            self.preLabels[self.totalPreviewImages-1].label.setPixmap(pixmap)
+            self.preLabels[self.totalPreviewImages-1].imageNr = newImgNr + math.floor(self.totalPreviewImages/2)
+        else:
+            pix = QPixmap(self.previewImageSize.x(), self.previewImageSize.y())
+            pix.fill(QColor(0,0,0,0))
+            self.preLabels[self.totalPreviewImages-1].label.setPixmap(pix)
+            self.preLabels[self.totalPreviewImages-1].imageNr = 0
+
+        # === DEBUG ===
+        pstr = ""
+        for i in range(len(self.preLabels)):
+            pstr += str(self.preLabels[i].imageNr) + " "
+        print("Scrolling finished. Position is: " + pstr)
+        
+      
+     
+        
+              
 
     def setupSignals(self):
         pass
+    
