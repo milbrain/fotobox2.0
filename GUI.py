305d5a3542ff1ffde4b5a66957958067187c13de
Bartosz Milejski <mil_bar@hotmail.de>
2019-05-15 13:03:53 +0200

    Add explanation box for howto connect to Pi and stuff, currently filled with lorem ipsum

--- GUI.py
+++ GUI.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-import logging, qrcode, os.path, math
+import logging, qrcode, os.path, math, lorem
 
 from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtGui import QImage
@@ -12,56 +12,72 @@ class Record:
     pass
 
 class GUI(QMainWindow):
-    
+
     log = logging.getLogger(__name__)   # the logger
-    
+
     qrFilepath      = 'res/qr/'
     imageFilepath   = 'res/img/'
     previewFilepath = 'res/preview/'
-    
+
     formSize = QPoint(1000,700)         # main form
-    
+
     previewImageSize = QPoint(165, 165)
     totalPreviewImages = 5
-    
+
     bigImageSize    = None              # is only set after the first call to _setupBigImage
-    
+
     bigLabel        = None              # Record with reference to a QLabel and the ImageNr currently stored there
                                         # only available after _setupBigImage
     preLabels       = []                # List of tuples of the same form as bigLabel, just for the preview labels
                                         # only available after _setupPreviewImages
-    
+
     def __init__(self, cont):
         super().__init__()
-        self.controls = cont          
+        self.controls = cont
         self.initUI()
         self.setupSignals()
 
 
     def initUI(self):
         ''' Inits all the UI elements on the main form . '''
-        # Params start 
+        # Params start
         canvasSize = QPoint(100,100)        # canvas to draw on
-        # Params end 
-        
+        # Params end
+
         # Main Window
         self.resize(self.formSize.x(), self.formSize.y())
         self.setGeometry(QtWidgets.QStyle.alignedRect(QtCore.Qt.LeftToRight, QtCore.Qt.AlignCenter,
                                                       self.size(), QApplication.desktop().availableGeometry()))
         self.setWindowTitle('Fotobox v2.0')
         self.show()
-        
+
         self._setupBigImage()
         self._setupPreviewImages()
         self._setupQRCode()
+        self._setupExplanation()
+
         self.grabKeyboard()     # grabs all keyboard inputs to be handled for scrolling through preview images
-        
+
+    def _setupExplanation(self):
+        ''' Sets up the HOWTO connect to the Pi to download a picture via QR code. '''
+        expSize = QPoint(165,250)
+        size = QRect(self.qrlabel.pos().x(), self.qrlabel.pos().y() + self.qrlabel.size().height() + 10 , expSize.x(), expSize.y())
+        self.explabel = QtWidgets.QLabel(self)
+        self.explabel.setGeometry(size)
+        self.explabel.setAlignment(Qt.AlignJustify)
+        self.explabel.setWordWrap(True)
+        asdf = lorem.paragraph()
+        self.explabel.setText("passwd: catchfoto \n" + asdf)
+
+
+        self.explabel.show()
+
     def keyPressEvent(self, event):
         if event.key() == QtCore.Qt.Key_Left:
             self.scrollLeft()
         elif event.key() == QtCore.Qt.Key_Right:
             self.scrollRight()
-            
+
     def _setupPreviewImages(self):
         ''' Loads all preview images into the form. '''
         formWidth = self.formSize.x()
@@ -69,35 +85,35 @@ class GUI(QMainWindow):
         preWidth = self.previewImageSize.x()
         preHeight = self.previewImageSize.y()
         existingImages = 0
-        
+
         imgDir = QDir(self.previewFilepath)
         existingImages = imgDir.count() - 2
-        
+
         self.preLabels = []
         inBetweenSpace = (formWidth - 20 - preWidth*self.totalPreviewImages) / (self.totalPreviewImages-1)
-        
+
         # Which image should be loaded first starting from left to right?
         imgToGenerate = existingImages - math.floor(self.totalPreviewImages/2)
         if imgToGenerate < 1:
             # Only few images exist in preview folder (in standard configuration: 2 or less)
             # We work with what we've got
             imgToGenerate = 1
-        
+
         for i in range(0,self.totalPreviewImages,1):
             # Create as many labels as requested in totalPreviewImages
             size = QRect(10 + i*preWidth + inBetweenSpace*i, formHeigth - preHeight - 10,
                          preWidth, preHeight)
-            
+
             preview = Record()
             preview.label = QtWidgets.QLabel(self)
             preview.imageNr = 0
-            
+
             self.preLabels.append(preview)
             self.preLabels[i].label.setGeometry(size)
             self.preLabels[i].label.setAlignment(Qt.AlignCenter)
             self.preLabels[i].label.setFrameShape(QFrame.NoFrame)
             self.preLabels[i].label.setLineWidth(0)
-            
+
             # Iteratively try to generate the next picture
             pixmap = self.generatePreviewImage(imgToGenerate)
             if pixmap:
@@ -106,12 +122,12 @@ class GUI(QMainWindow):
             else:
                 # No more pictures left to load
                 self.preLabels[i].imageNr = 0
-                
+
             self.preLabels[i].label.show()
             imgToGenerate += 1
-        
+
         self._highlightActivePreview()
-        
+
         # === DEBUG ===
         pstr = ""
         for i in range(len(self.preLabels)):
@@ -128,7 +144,7 @@ class GUI(QMainWindow):
         if os.path.isfile(prePath):
             pixmap = QtGui.QPixmap(prePath)
             return pixmap
-        
+
         img = QImage(fullImgPath)
         pixmap = QtGui.QPixmap.fromImage(img.scaled(self.previewImageSize.x(), self.previewImageSize.y(),
                                                     Qt.KeepAspectRatio, Qt.SmoothTransformation))
@@ -141,50 +157,50 @@ class GUI(QMainWindow):
         bigHeight = self.formSize.y() - self.previewImageSize.y() - 10 - 20
         size = QRect(10, 10, bigWidth, bigHeight)
         self.bigImageSize = QPoint(bigWidth, bigHeight)
-        
+
         imgDir = QDir(self.previewFilepath)
         existingImages = imgDir.count() - 2
-        
+
         self.bigLabel = Record()
         self.bigLabel.label = QLabel(self)
         self.bigLabel.imageNr = 0
         self.bigLabel.label.setGeometry(size)
         self.bigLabel.label.setAlignment(Qt.AlignCenter | Qt.AlignVCenter)
         self.bigLabel.label.show()
-        self.loadBigImage(existingImages, bigWidth, bigHeight)   
-        
+        self.loadBigImage(existingImages, bigWidth, bigHeight)
+
     def loadBigImage(self, imgNr, w, h):
         ''' Loads the big preview label with the existing image referenced by imgNr. '''
         # If permance is too low, change QtSmoothTransformation
         filepath = self.imageFilepath + 'P' + str(imgNr) + '.jpg'
         if not os.path.isfile(filepath):
             return False
-        
+
         img = QImage(filepath)
         pixmap = QtGui.QPixmap.fromImage(img.scaled(w, h, Qt.KeepAspectRatio, Qt.SmoothTransformation))
         self.bigLabel.label.setPixmap(pixmap)
         self.bigLabel.imageNr = imgNr
-        return True    
+        return True
 
     def _highlightActivePreview(self):
         ''' Change the border of the preview labels to adequately highlight the image
         currently beeing displayed in bigLabel. '''
         highlightWidth = 3
         noHighlightWidth = 0
-        
+
         for i in range(self.totalPreviewImages):
             # If the current image is not highlighted, but should be
             if (self.preLabels[i].label.lineWidth() == noHighlightWidth and
                     self.bigLabel.imageNr == self.preLabels[i].imageNr):
                 self.preLabels[i].label.setLineWidth(highlightWidth)
                 self.preLabels[i].label.setFrameShape(QFrame.Panel)
-            
+
             # If the current image is highlighted, but shouldn't be
             if (self.preLabels[i].label.lineWidth() == highlightWidth and not
                     self.bigLabel.imageNr == self.preLabels[i].imageNr):
                 self.preLabels[i].label.setLineWidth(noHighlightWidth)
                 self.preLabels[i].label.setFrameShape(QFrame.NoFrame)
-       
+
     def _setupQRCode(self):
         ''' Create the label to show the QR Code for and load the first QR-Code (if at least
         one image exists in the image folder already). '''
@@ -193,10 +209,10 @@ class GUI(QMainWindow):
         size = QRect(self.formSize.x() - qrSize.x() - 10, 10, qrSize.x(), qrSize.y())
         self.qrlabel = QtWidgets.QLabel(self)
         self.qrlabel.setGeometry(size)
-        
+
         self.generateOrLoadQR(self.bigLabel.imageNr)
         self.qrlabel.show()
-        
+
     def generateOrLoadQR(self, imgNr):
         ''' Loads an existing QR-Code into the UI or generates the QR Code first and
         loads it afterwards. Parameter denotes the image number to load the QR code for.'''
@@ -214,23 +230,23 @@ class GUI(QMainWindow):
             qr.make(fit=True)
             img = qr.make_image()
             img.save(filepath)
-        
+
         pixmap = QtGui.QPixmap(filepath)
         self.qrlabel.setPixmap(pixmap)
-                         
+
     def scrollLeft(self):
         newImgNr = self.bigLabel.imageNr - 1
-        
+
         # bigPreviewImage
         if newImgNr <= 0 or not self.loadBigImage(newImgNr, self.bigImageSize.x(), self.bigImageSize.y()):
             return
-        
+
         # QR-Code
         self.generateOrLoadQR(newImgNr)
-        
+
         # set new Image Number because everything worked out
         self.bigLabel.imageNr = newImgNr
-        
+
         # Realign small preview images (if neccessary)
         imgDir = QDir(self.imageFilepath)
         existingImages = imgDir.count() - 2
@@ -238,7 +254,7 @@ class GUI(QMainWindow):
             existingImages <= self.totalPreviewImages):
             self._highlightActivePreview()
             return
-        
+
         for i in range(self.totalPreviewImages-1, 0,-1):
             pixmap = self.preLabels[i-1].label.pixmap()
             if pixmap:
@@ -250,9 +266,9 @@ class GUI(QMainWindow):
         if pixmap:
             self.preLabels[0].label.setPixmap(pixmap)
             self.preLabels[0].imageNr = newImgNr - math.floor(self.totalPreviewImages/2)
-        
+
         self._highlightActivePreview()
-        
+
         # === DEBUG ===
         pstr = ""
         for i in range(len(self.preLabels)):
@@ -261,17 +277,17 @@ class GUI(QMainWindow):
 
     def scrollRight(self):
         newImgNr = self.bigLabel.imageNr + 1
-        
+
         # bigPreviewImage
         if newImgNr <= 0 or not self.loadBigImage(newImgNr, self.bigImageSize.x(), self.bigImageSize.y()):
             return
-        
+
         # QR-Code
         self.generateOrLoadQR(newImgNr)
-        
+
         # set new Image Number because everything worked out
         self.bigLabel.imageNr = newImgNr
-        
+
         # Realign small preview images (if neccessary)
         imgDir = QDir(self.imageFilepath)
         existingImages = imgDir.count() - 2
@@ -279,7 +295,7 @@ class GUI(QMainWindow):
             existingImages <= self.totalPreviewImages):
             self._highlightActivePreview()
             return
-        
+
         for i in range(0,self.totalPreviewImages-1, 1):
             pixmap = self.preLabels[i+1].label.pixmap()
             if pixmap:
@@ -303,14 +319,13 @@ class GUI(QMainWindow):
             self.preLabels[self.totalPreviewImages-1].imageNr = 0
 
         self._highlightActivePreview()
-        
+
         # === DEBUG ===
         pstr = ""
         for i in range(len(self.preLabels)):
             pstr += str(self.preLabels[i].imageNr) + " "
         print("Scrolling finished. Position is: " + pstr)
-  
+
 
     def setupSignals(self):
         pass
-    
--- main.py
+++ main.py
@@ -9,32 +9,32 @@ import Server, GUI
 import sys, logging, threading
 
 class Controls(QObject):
-    
+
     ip = '127.0.0.222'                      # use dnsmasq to create an alias in /etc/hosts file
     dnsString = 'get.raspberry.images'  # current dnsmasq alias for self.ip
-    
+
     def initServer(self):
         ''' Inits the server that is used to communicate with the webapp, sets up
         the signals needed for the communication and starts the server thread. '''
         port = 1337
-        
+
         server_address = (self.ip, port)
         self.server = Server.MyServer(server_address, Server.RequestHandler)
         self.server.handler = Server.RequestHandler
         self.server.handler.mainref = self
-        
+
         # signals for safe inter-thread communication
         #self.server.returnSignal.connect(self.i_plotterReturn)
         #self.server.changePrintModeSignal.connect(self.i_changePrintMode)
         #self.server.plotterDrawSignal.connect(self.i_plotterDraw)
         #self.server.plotterGotoSignal.connect(self.i_plotterGotoXY)
-        
+
         self.server.startThread()
         print("Server started and listening on port %s." % port)
-        
+
     def initForm(self):
         self.gui = GUI.GUI(self)
-    
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     controls = Controls()
